c2a3c7f312da728e959e6c44142171bd3d9d7f0f
Heba Abdulal <hebaabdulal24@gmail.com>
2024-04-04 19:54:26 +0000

    Added function get_income_data() to gather income data from the user. Implemented a while loop to continuously prompt the user for input until valid data is provided. Displayed instructions for the user on the required format of the income data. Provided an example to illustrate the expected format of the input data.

--- run.py
+++ run.py
@@ -12,3 +12,14 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('fin-tracker')
+
+def get_income_data():
+
+    """
+    Get income data from the user
+    """
+
+    while True:
+        print("Please enter income data.")
+        print("Data should be three values: amount, description, date.")
+        print("Example: 2000, Salary, 2024-04-01")
